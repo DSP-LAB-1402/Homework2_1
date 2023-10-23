fe9aec28650f0a75e6eefb8b352ec1f99461848c
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-10-23 13:59:00 +0330

    Declare Clear workspace

--- Homework2.m
+++ Homework2.m
@@ -1,1 +1,13 @@
-%%% * Homework2*
+%% * Homework2*
+%% Programmers
+%%%
+% Mohammad Mahdi Elyasi - 9823007
+%
+% Moein Nasiri - 9823093
+
+%% Clear the Workspace
+close all;
+clear all;
+clc;
+
+%% Homework2_1
