10087171a727e5c84e3a20d94c101c71a755a44e
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-10-25 00:26:32 +0330

    Finish Homework2_2

--- Homework2.m
+++ Homework2.m
@@ -10,4 +10,22 @@ close all;
 clear all;
 clc;
 
-%% Homework2_1
+%% Homework2_2
+h = 0.1*ones(1,10);
+t = 0:1:199;
+x = [ones(1,25) zeros(1,25)];
+for i = 1:3
+    x = [x ones(1,25) zeros(1,25)];
+end
+y = myconv(x,h);
+y=y(1:200);
+
+figure('Name', 'Convolution');
+stem(t,x);
+xlabel('Samples');
+ylabel('Amplitude');
+title('Signals');
+grid on;
+hold on;
+stem(t,y);
+legend('input', 'Convolved signal');
